b212782bca13a20f2e53b50aea1bf7cdb5c488a6
BOGDFPCSRFOD126\Aprendiz <107689067+Daniel7694@users.noreply.github.com>
2024-03-07 17:49:04 -0500

    correccion enpoints

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,10 +1,93 @@
 const express = require('express');
 const Usuario = require('../models/usuario_model');
+const Joi = require('@hapi/joi');
 const ruta = express.Router();
 
 ruta.get('/', (req, res) => {
     res.json('respuesta a la peticion GET de CURSOS correctamente...')
 });
+
+
+// Validacones para el objeto usuario
+const schema = Joi.object({
+    nombre: Joi.string()
+        .min(3)
+        .max(30)
+        .required()
+        .pattern(/^[A-Za-záéíóú ]{3,30}$/),
+
+    password: Joi.string()
+        .pattern(/^[a-zA-Z0-9]{3,30}$/),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'edu', 'co'] } })
+});
+
+
+//Enpoint de tipo POST paraa el recurso USUARIOS
+ruta.post('/', (req, res) => {
+    let body = req.body;
+
+    const { error, value } = schema.validate({ nombre: body.nombre, email: body.email });
+    if (!error) {
+        let resultado = crearUsuario(body);
+
+        resultado.then(user => {
+            res.json({
+                valor: user
+            })
+        }).catch(err => {
+            res.status(400).json({
+                err
+            })
+        });
+    } else {
+        res.status(400).json({
+            error
+        })
+    }
+});
+
+//Enpoint de tipo POST para actualizar los datos del usuario
+ruta.put('/:email', (req, res) => {
+    const {error, value} = schema.validate({ email: req.body.nombre});
+    if(!error) {
+        let resultado = actualizarUsuario(req.params.email, req.body);
+        resultado.then(valor => {
+            res.json({
+                valor
+            })
+        }).catch(err => {
+            res.status(400).json({
+                err
+            })
+        });
+    } else {
+        res.status(400).json({
+            error
+        })
+    }
+});
+// Funcion asincrona para crear un objeto de tipo usuarrio
+async function crearUsuario(body) {
+    let usuario = new Usuario({
+        email: body.email,
+        nombre: body.nombre,
+        password: body.password
+    });
+    return await usuario.save();
+}
+
+async function actualizarUsuario(email, body){
+    let usuario = await Usuario.findOneAndUpdate({"email": email}, {
+        $set: {
+        nombre: body.nombre,
+        password: body.password
+        }
+
+    }, {new: true});
+    return usuario;
+}
 
 
 module.exports = ruta;
