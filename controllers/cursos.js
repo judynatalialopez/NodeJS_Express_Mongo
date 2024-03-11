c94e65c90aabab6c75410500fa84d05d8bf57bcb
judynatalialopez <137812850+judynatalialopez@users.noreply.github.com>
2024-03-10 20:45:06 -0500

    agregando y probando la funcionalidad para editar cursos. Enpoint de tipo PUT

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -8,5 +8,51 @@ ruta.get('/', (req, res) => {
 
 
 
+//ENdpointde tipo POST  para el recurso Cursos 
+ruta.post('/', (req, res) =>{
+    let resultado = crearCurso(req.body);
+
+    resultado.then(curso => {
+        res.json({
+            curso
+        })
+    }).catch(err => {
+        res.status(400).json({
+            err
+        })
+    })
+});
+
+//Endpoint de tipo Put para el rescurso Cursos 
+ruta.put('/:id', (req, res) => {
+    let resultado = actualizarCurso(req.params.id, req.body);
+    resultado.then(curso => {
+        res.json(curso)
+    }).catch(err => {
+        res.status(400).json(err)
+    })
+});
+
+// Funcion asincrona para crear cursos 
+async function crearCurso(body){
+    let curso = new Curso({
+        titulo : body.titulo,
+        descripcion  : body.descripcion,
+        alumnos : body.alumnos,
+        calificacion : body.calificacion
+    });
+    return await curso.save();
+}
+
+// Funcion asincrona para actualizar cursos
+async function actualizarCurso(id, body){
+    let curso = await Curso.findByIdAndUpdate(id, {
+        $set: {
+            titulo: body.titulo,
+            descripcion: body.descripcion
+        }
+    }, {new: true});
+    return curso;
+}
 
 module.exports = ruta;
